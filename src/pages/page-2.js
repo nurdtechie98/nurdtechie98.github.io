f362f4e7517437ef824f6b632ae424db452f6d6f
nurdtechie98 <nurdtechie98@gmail.com>
2019-01-14 23:04:31 +0530

    add second page

--- src/pages/page-2.js
+++ src/pages/page-2.js
@@ -5,8 +5,10 @@ import Layout from '../components/layout'
 
 const SecondPage = () => (
   <Layout>
-    <h1>Hi from the second page</h1>
-    <p>Welcome to page 2</p>
+    <h1>NLT-GITHUB: KWOC 2019</h1>
+    <p>
+
+    </p>
     <Link to="/">Go back to the homepage</Link>
   </Layout>
 )
