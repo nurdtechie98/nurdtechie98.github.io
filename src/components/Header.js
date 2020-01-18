bdf29e62d0c90b977a41461ece821d0fc065ccff
nurdtechie98 <nurdtechie98@gmail.com>
2020-01-18 13:22:30 +0530

    add drive-cli privacy page

--- src/components/Header.js
+++ src/components/Header.js
@@ -10,7 +10,7 @@ const Header = (props) => (
             <div className="inner">
                 <h1>Chirag Shetty</h1>
                 <a href='https://www.google.co.in/search?q=nurdtechie98'><h2 className='box'>nurdtechie98</h2></a>
-                <p>Tech enthusiast | Junior Year CS undergrad | Developer</p>
+                <p>Tech enthusiast | Senior Year CS undergrad | Developer</p>
             </div>
         </div>
         <nav>
--- src/components/Main.js
+++ src/components/Main.js
@@ -12,11 +12,12 @@ class Main extends React.Component {
 
         <article id="intro" className={`${this.props.article === 'intro' ? 'active' : ''} ${this.props.articleTimeout ? 'timeout' : ''}`} style={{display:'none'}}>
           <h2 className="major">About Me</h2>
-          <p>I am currently a junior year undergrad student in the Department of <strong>Computer Science</strong> at K.J Somaiya College Of Engineering , Mumbai .</p>
+          <p>I am currently a senior year undergrad student in the Department of <strong>Computer Science</strong> at K.J Somaiya College Of Engineering , Mumbai . </p>
           <p>Over the period of past years, I have spent a lot of my time solving problems on <a href='https://www.codechef.com/users/nurdtechie98'>codechef</a> , <a href='https://www.codechef.com/users/nurdtechie98'>codeforces</a> and other <strong>competitive programming</strong> websites.</p>
-          <p>Building new products and exploring new tech is what I love doing thus, <strong>hackathons</strong> are events that I don't miss by any chance. Along with a peer group I have been to several hackathon but a few notable one's would be <ul><li><a href='https://hackinout.co/'>Hack InOut 2018</a></li><li><a href='https://mumbaihackathon.in'>Mumbai Hackathon 2018</a></li></ul> </p>
-          <p>Apart from this, from the past two years I am an active member at my <a href='http://kjscecodecell.com'><strong>campus programming club</strong></a> and work with a group of amazing minds.</p>
-          <p>Heres my complete <a href={Pdf}>resume</a></p>
+          <p>Building new products and exploring new tech is what I love doing thus, <strong>hackathons</strong> are events that I don't miss by any chance. At the same time I have also done my bit of <strong>Open Source</strong> contributions.</p>
+          <p>Currently, I am looking for developer roles at an organisation which has room for learning and the same time values my contribution.</p> 
+          <p>Feel like hiring me. Heres my complete <a href={Pdf}>resume</a></p>
+          <p>I would love connecting with you :)</p>
           {close}
         </article>
 
@@ -59,6 +60,11 @@ class Main extends React.Component {
         <article id="about" className={`${this.props.article === 'about' ? 'active' : ''} ${this.props.articleTimeout ? 'timeout' : ''}`} style={{display:'none'}}>
           <h2 className="major">Work Experience</h2>
           <div className="box">
+            <p><strong>TIAA GBS</strong> | SOFTWARE DEVELOPER INTERN</p>
+            <pre>JUNE 2019 – AUG 2019</pre>
+            <p>Worked in the automation team to build an In-house automation framework. Added capabilities of sauceLabs, selenium grid to UI testing. Developed Utility using hibernate for Database Testing </p>
+          </div>
+          <div className="box">
             <p><strong>FRAPPE</strong> | STUDENT DEVELOPER</p>
             <pre>June 2018 – Aug 2018</pre>
             <p>One of the 20 students selected for ESOC program where we developed an erp system, It is completely open source project. Currently it is set for release beta release in 2019</p>
@@ -66,12 +72,7 @@ class Main extends React.Component {
           <div className="box">
             <p><strong>Codecell</strong> | COMMITTEE HEAD</p>
             <pre>June 2017 – June 2019</pre>
-            <p>Currently the committee head of the official coding council of the college heading a team of 15 members. The aim of the council is to promoting competitive coding and development culture across the college by arranging workshops, meetups, contests and hackathon</p>
-          </div>
-          <div className="box">
-            <p><strong>SOMAIYA R&D CELL</strong> | IN-HOUSE INTERN</p>
-            <pre>NOV 2018 – DEC 2018</pre>
-            <p>worked on building a SMS broadcast system to help farmers improve their yield by suggesting appropriate water amount and fertilizers specific to their region. collaborated with team of electronics branch student working on IOT sensors to analyze the data</p>
+            <p>Lead the official coding council of the college heading a team of 15 members. The aim of the council is to promoting competitive coding and development culture across the college by arranging workshops, meetups, contests and hackathon</p>
           </div>
           {close}
         </article>
